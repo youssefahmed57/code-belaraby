8e96fe78433d99776c329692478eefa350ff10f3
EljokesX <youssefalraghay@gmail.com>
2026-08-08 03:39:56 +0300

    Remove withCredentials from axios to allow unblocked cross-origin HTTP requests in Chrome

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -6,7 +6,6 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://a12tqtb1zoht2490
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
